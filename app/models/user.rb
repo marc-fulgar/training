3c149caed0bc21c246156a21b89c7bfe1143429a
marc-fulgar <marc.fulgar@adish.co.jp>
2017-07-04 14:43:46 +0800

    Devise Revise - Fixed Username Uniqueness and Presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         
+         :recoverable, :rememberable, :trackable, :validatable  
+  
   attr_accessor :login
+  validates :username, :presence => true, :uniqueness => true
   
   def email_required?
     false
