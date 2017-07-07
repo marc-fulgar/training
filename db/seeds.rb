d1a0dcd5e1c34a866328c56c232b98f07d637b63
marc-fulgar <marc.fulgar@adish.co.jp>
2017-07-07 12:08:10 +0800

    Revised Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-(1..50).each do |i|
-  first_name = Faker::Name.first_name
-  last_name  = Faker::Name.last_name
-  email      = Faker::Internet.free_email("#{first_name} #{last_name}")
-  position   = Faker::Name.title
-  User.create(first_name: first_name, last_name: last_name, email: email, position: position)
+(1..5).each do |i|
+  name = "Department #{i}"
+  Department.create(name: name)
+end
+
+(1..20).each do |i|
+  first_name    = Faker::Name.first_name
+  username      = "first_name #{i}"
+  last_name     = Faker::Name.last_name
+  email         = Faker::Internet.free_email("#{first_name} #{last_name}")
+  department_id = (i%5 + 1)
+  position      = Faker::Name.title
+  user = User.new(
+    username: username,
+    first_name: first_name,
+    last_name: last_name, 
+    email: email, 
+    department_id: department_id,
+    position: position,
+    password: "password")
+  if user.save
+    puts "success!"
+  else
+    puts user.errors.messages
+  end
+end
+
+
+(1..20).each do |i|
+  content = "Hello world number #{i}"
+  user_id = (21-i)
+  Post.create(content: content, user_id: user_id)
 end
