9a20d9201d2023c4dc4b065f24a979ffb0857f6a
marc-fulgar <marc.fulgar@adish.co.jp>
2017-07-19 08:38:34 +0800

    Clickable headers to sort works, but department is not associated yet

--- app/assets/javascripts/components/user_list.js.jsx
+++ app/assets/javascripts/components/user_list.js.jsx
@@ -1,16 +1,20 @@
 var UserList = React.createClass({
-
   render: function() {
+		a = this.props.a;
+		b = this.props.b;
+		c = this.props.c;
+		d = this.props.d;
+		e = this.props.e;
     return (
       <div className="table-responsive">
         <table data-toggle="table" className="table table-striped">
           <thead>
             <tr>
-              <th>Last name</th>
-              <th>First name</th>
-							<th>Department</th>
-              <th>Position</th>
-              <th>Email</th>
+              <th><a href={a}>Last name</a></th>
+              <th><a href={b}>First name</a></th>
+							<th><a href={c}>Department</a></th>
+              <th><a href={d}>Position</a></th>
+              <th><a href={e}>Email</a></th>
             </tr>
           </thead>
           <tbody>
